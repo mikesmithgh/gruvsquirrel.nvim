e178a49767b1dd6e654148d87b57bb956f6cbdf3
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2023-03-08 14:48:58 -0500

    chore: dev stuff

--- lua/gruvsquirrel/dev/init.lua
+++ lua/gruvsquirrel/dev/init.lua
@@ -1,3 +1,18 @@
--- function hiya()
---
--- end
+local M = {}
+
+local hex = function(n)
+  if n == nil then
+    return "no color"
+  end
+  return string.format("#%06x", n)
+end
+
+M.something = function()
+  local cword = vim.fn.expand('<cword>')
+  local hl = vim.api.nvim_get_hl_by_name(cword, true)
+  vim.notify(cword, vim.log.levels.INFO, {})
+  vim.notify(vim.inspect(hl), vim.log.levels.INFO, {})
+  vim.notify(hex(hl['foreground']), vim.log.levels.INFO, {})
+end
+
+return M
