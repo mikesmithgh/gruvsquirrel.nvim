6c4f1feda84142bfd551e221524e6fa3a0907565
Mike <10135646+mikesmithgh@users.noreply.github.com>
2023-07-23 08:56:48 -0400

    chore: add mini.sh from fzf-lua (#91)

---
+++ scripts/init.lua
@@ -0,0 +1,9 @@
+local plugin_name = 'gruvsquirrel.nvim'
+-- add temp path from scripts/mini.sh in case this is running locally
+local tempdir = vim.trim(vim.fn.system([[sh -c "dirname $(mktemp -u)"]]))
+local packpath = os.getenv("PACKPATH") or tempdir .. "/" .. plugin_name .. ".tmp/nvim/site"
+
+vim.cmd("set packpath=" .. packpath)
+vim.o.termguicolors = true
+require "gruvsquirrel".setup()
+vim.cmd([[colorscheme gruvsquirrel]])
