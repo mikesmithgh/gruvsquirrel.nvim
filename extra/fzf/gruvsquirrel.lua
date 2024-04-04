79873001e588d87144a43c01f43df0d9e67c0fe8
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-04-03 22:23:36 -0400

    chore: zen mode and incline.nvim

--- extra/fzf/gruvsquirrel.lua
+++ extra/fzf/gruvsquirrel.lua
@@ -1,5 +1,4 @@
-vim.schedule(
-  function()
-    vim.print(require('gruvsquirrel.plugins.fzf-lua').fzf_colors_str())
-  end
-)
+vim.opt.eventignore:append('all')
+vim.schedule(function()
+  vim.print(require('gruvsquirrel.plugins.fzf-lua').fzf_colors_str())
+end)
---
+++ lua/gruvsquirrel/highlight-groups/zen-mode.lua
@@ -0,0 +1,12 @@
+local with_meta = require("gruvsquirrel.util.tbl").with_meta
+
+--- Undotree Highlight Groups
+---@class GruvsquirrelZenModeGroup
+local M = {
+  ZenBg = "ZenBg",
+}
+
+---@type GruvsquirrelZenModeGroup
+M = with_meta(M)
+
+return M
