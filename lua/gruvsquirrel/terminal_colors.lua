cc19b680c62dd6e3dcfe39c337431e63f0a55449
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2025-01-09 15:56:10 -0500

    fix: add missing _ to terminal colors

--- lua/gruvsquirrel/terminal_colors.lua
+++ lua/gruvsquirrel/terminal_colors.lua
@@ -12,27 +12,27 @@ local M = {
 
     -- green
     vim.g.terminal_color_2 = c.doreen_0
-    vim.g.terminal_color10 = c.doreen_8
+    vim.g.terminal_color_10 = c.doreen_8
 
     -- yellow
     vim.g.terminal_color_3 = c.sandy_0
-    vim.g.terminal_color11 = c.hammy_0
+    vim.g.terminal_color_11 = c.hammy_0
 
     -- blue
     vim.g.terminal_color_4 = c.chitter_0
-    vim.g.terminal_color12 = c.chitter_1
+    vim.g.terminal_color_12 = c.chitter_1
 
     -- purple (magenta)
     vim.g.terminal_color_5 = c.surly_0
-    vim.g.terminal_color13 = c.surly_1
+    vim.g.terminal_color_13 = c.surly_1
 
     -- blue_green (cyan)
     vim.g.terminal_color_6 = c.skippy_0
-    vim.g.terminal_color14 = c.skippy_1
+    vim.g.terminal_color_14 = c.skippy_1
 
     -- light (white)
     vim.g.terminal_color_7 = c.sally_0
-    vim.g.terminal_color15 = c.surly_2
+    vim.g.terminal_color_15 = c.surly_2
   end,
 }
 return M
