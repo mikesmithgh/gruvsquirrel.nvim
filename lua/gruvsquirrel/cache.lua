22bbd351f15a154a80e503dfeb5ae4e38ba09509
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2025-01-09 08:25:05 -0500

    feat: add caching to improve startup times

---
+++ lua/gruvsquirrel/cache.lua
@@ -0,0 +1,41 @@
+local M = {}
+
+local uv = vim.uv or vim.loop
+
+local function write_file(file, contents)
+  vim.fn.mkdir(vim.fn.fnamemodify(file, ':h'), 'p')
+  local fd = assert(io.open(file, 'w+'))
+  fd:write(contents)
+  fd:close()
+end
+
+local function read_file(file)
+  local fd = assert(io.open(file, 'r'))
+  ---@type string
+  local data = fd:read('*a')
+  fd:close()
+  return data
+end
+
+function M.file(key)
+  return vim.fn.stdpath('cache') .. '/gruvsquirrel-' .. key .. '.json'
+end
+
+function M.read(key)
+  local ok, ret = pcall(function()
+    return vim.json.decode(read_file(M.file(key)))
+  end)
+  return ok and ret or nil
+end
+
+function M.write(key, data)
+  pcall(write_file, M.file(key), vim.json.encode(data))
+end
+
+function M.clear()
+  for _, style in ipairs({ 'gruvsquirrel', 'boxsquirrel' }) do
+    uv.fs_unlink(M.file(style))
+  end
+end
+
+return M
