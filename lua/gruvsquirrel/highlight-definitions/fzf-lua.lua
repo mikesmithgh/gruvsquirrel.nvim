26375c25c469db5f598413f472e8ec980c26db16
Mike <10135646+mikesmithgh@users.noreply.github.com>
2025-02-16 21:01:28 -0500

    fix: add highlight for FzfLuaTitleFlags (#108)

--- lua/gruvsquirrel/highlight-definitions/fzf-lua.lua
+++ lua/gruvsquirrel/highlight-definitions/fzf-lua.lua
@@ -1,5 +1,63 @@
-local g = require('gruvsquirrel.highlight-groups.fzf-lua')
+--- See https://github.com/ibhagwan/fzf-lua/blob/main/lua/fzf-lua/init.lua
+
+--- FzfLua Highlight Groups
+---@class GruvsquirrelFzfLuaGroup
+---@field FzfLuaBorder string
+---@field FzfLuaBufName string
+---@field FzfLuaCursor string
+---@field FzfLuaCursorLine string
+---@field FzfLuaCursorLineNr string
+---@field FzfLuaHelpBorder string
+---@field FzfLuaHelpNormal string
+---@field FzfLuaNormal string
+---@field FzfLuaPathColNr string
+---@field FzfLuaPathLineNr string
+---@field FzfLuaPreviewBorder string
+---@field FzfLuaPreviewNormal string
+---@field FzfLuaScrollBorderEmpty string
+---@field FzfLuaScrollBorderFull string
+---@field FzfLuaScrollFloatEmpty string
+---@field FzfLuaScrollFloatFull string
+---@field FzfLuaSearch string
+---@field FzfLuaTitle string
+---@field FzfLuaTitleFlags string
+
+-- TODO: add highlights for the following groups
+-- ---@field FzfLuaBackdrop string
+-- ---@field FzfLuaPreviewTitle string
+-- ---@field FzfLuaHeaderBind string
+-- ---@field FzfLuaHeaderText string
+-- ---@field FzfLuaBufId string
+-- ---@field FzfLuaBufNr string
+-- ---@field FzfLuaBufLineNr string
+-- ---@field FzfLuaBufFlagCur string
+-- ---@field FzfLuaBufFlagAlt string
+-- ---@field FzfLuaTabTitle string
+-- ---@field FzfLuaTabMarker string
+-- ---@field FzfLuaDirIcon string
+-- ---@field FzfLuaDirPart string
+-- ---@field FzfLuaFilePart string
+-- ---@field FzfLuaLivePrompt string
+-- ---@field FzfLuaLiveSym string
+-- ---@field FzfLuaFzfNormal string
+-- ---@field FzfLuaFzfCursorLine string
+-- ---@field FzfLuaFzfMatch string
+-- ---@field FzfLuaFzfBorder string
+-- ---@field FzfLuaFzfScrollbar string
+-- ---@field FzfLuaFzfSeparator string
+-- ---@field FzfLuaFzfGutter string
+-- ---@field FzfLuaFzfHeader string
+-- ---@field FzfLuaFzfInfo string
+-- ---@field FzfLuaFzfPointer string
+-- ---@field FzfLuaFzfMarker string
+-- ---@field FzfLuaFzfSpinner string
+-- ---@field FzfLuaFzfPrompt string
+-- ---@field FzfLuaFzfQuery string
+
+---@type GruvsquirrelFzfLuaGroup
+local g = require('gruvsquirrel.util.tbl').with_meta()
 local mapper = require('gruvsquirrel.util.mapper')
+
 local M = {
   attributes = function()
     local d = require('gruvsquirrel.common.highlight_aliases').get()
@@ -23,10 +81,12 @@ local M = {
       { { g.FzfLuaScrollFloatEmpty }, d.bg_blue3 },
       { { g.FzfLuaScrollFloatFull }, d.bg_dark3 },
       { { g.FzfLuaTitle }, d.fg_dark6_bg_gray1_bold },
+      { { g.FzfLuaTitleFlags }, d.fg_dark1_bg_gray1_bold_italic },
       { { g.FzfLuaBufName }, d.fg_purple0 },
       { { g.FzfLuaPathLineNr }, d.fg_green0 },
       { { g.FzfLuaPathColNr }, d.fg_blue4 },
     })
   end,
 }
+
 return M
